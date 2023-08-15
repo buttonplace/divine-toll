6becdfd5afd67d97fbe4e9a2f2ca9c76b0ea4d76
TeaTrey <teatreydev@gmail.com>
2023-08-15 14:38:04 -0500

    Testing vercel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,6 +34,9 @@ export default function Home() {
             <LoadingSpinner />
             <LoadingSpinner />
             <LoadingSpinner />
+            <LoadingSpinner />
+            <LoadingSpinner />
+            <LoadingSpinner />
           </>
         )}
       </ul>
