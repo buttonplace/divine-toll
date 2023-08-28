eed3b927061f0146ff3cfbfa205d90940ecfcaeb
TeaTrey <teatreydev@gmail.com>
2023-08-27 20:24:15 -0500

    Working daisy nav

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl normal-case">daisyUI</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Link</a>
+          </li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="bg-primary p-2">
+                <li>
+                  <a>Link 1</a>
+                </li>
+                <li>
+                  <a>Link 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
