f6bcc704318d2fca0c7cb9e253da9770438e2b76
TeaTrey <teatreydev@gmail.com>
2023-08-29 02:16:41 -0500

    Merging

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -30,12 +30,59 @@ const Navigation = () => {
   const router = useRouter();
 
   const icons = {
-    chevron: <ChevronDown fill="currentColor" size={16} height={undefined} width={undefined} />,
-    scale: <Scale className="text-warning" fill="currentColor" size={30} height={undefined} width={undefined} />,
-    lock: <Lock className="text-success" fill="currentColor" size={30} height={undefined} width={undefined} />,
-    activity: <Activity className="text-secondary" fill="currentColor" size={30} height={undefined} width={undefined} />,
-    server: <Server className="text-success" fill="currentColor" size={30} height={undefined} width={undefined} />,
-    user: <TagUser className="text-danger" fill="currentColor" size={30} height={undefined} width={undefined} />,
+    chevron: (
+      <ChevronDown
+        fill="currentColor"
+        size={16}
+        height={undefined}
+        width={undefined}
+      />
+    ),
+    scale: (
+      <Scale
+        className="text-warning"
+        fill="currentColor"
+        size={30}
+        height={undefined}
+        width={undefined}
+      />
+    ),
+    lock: (
+      <Lock
+        className="text-success"
+        fill="currentColor"
+        size={30}
+        height={undefined}
+        width={undefined}
+      />
+    ),
+    activity: (
+      <Activity
+        className="text-secondary"
+        fill="currentColor"
+        size={30}
+        height={undefined}
+        width={undefined}
+      />
+    ),
+    server: (
+      <Server
+        className="text-success"
+        fill="currentColor"
+        size={30}
+        height={undefined}
+        width={undefined}
+      />
+    ),
+    user: (
+      <TagUser
+        className="text-danger"
+        fill="currentColor"
+        size={30}
+        height={undefined}
+        width={undefined}
+      />
+    ),
   };
   return (
     <NavigationMenu>
@@ -71,13 +118,13 @@ const ListItem = React.forwardRef<
         <a
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            "hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors",
             className,
           )}
           {...props}
         >
           <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+          <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
             {children}
           </p>
         </a>
