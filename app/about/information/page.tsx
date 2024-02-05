ead2237966edcb2737eb5dc4e3f97564c1bef4bb
TeaTrey <teatreydev@gmail.com>
2024-02-05 11:21:58 -0600

    Email display fix, correct link.

--- app/about/information/page.tsx
+++ app/about/information/page.tsx
@@ -173,7 +173,7 @@ export default async function MainPage() {
             If you have any questions, suggestions, or want to contribute,
             contact me at{" "}
             <a className="underline" href="mailto:teatreydev@gmail.com.com">
-              TeaTreyDev@divinetoll.com
+              TeaTreyDev@gmail.com
             </a>
           </h2>
           <h2 className="md:text-md px-4 py-2 text-sm font-medium text-muted-foreground">
